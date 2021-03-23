bf5261fc5f556efeacd987ff17b9378d5587413c
leo <leonardo.ncintra@outlook.com>
2021-03-23 11:42:09 -0300

    prepare tenanct

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from pessoa.validators import validate_cnpj
 
-# Create your models here.
+class ClubeDeTiro(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    cnpj = models.CharField('CPF', max_length=11, validators=[validate_cnpj], unique=True)
+    email = models.EmailField('e-mail', unique=True)
+    telefone = models.CharField(max_length=20)
+    data_cadastro = models.DateTimeField('criado em', auto_now_add=True)
+
+    class Meta:
+        db_table = 'clube_de_tiro'
+        verbose_name_plural = 'clubes de tiro'
+        verbose_name = 'clube de tiro'
+        ordering = ('-data_cadastro',)
+
+    def __str__(self):
+        return self.nome
--- pessoa/validators.py
+++ pessoa/validators.py
@@ -7,3 +7,10 @@ def validate_cpf(value):
 
     if len(value) != 11:
         raise ValidationError('CPF deve ter 11 números.', 'length')
+
+def validate_cnpj(value):
+    if not value.isdigit():
+        raise ValidationError('CNPJ deve conter apenas números.', 'digits')
+
+    if len(value) != 14:
+        raise ValidationError('CNPJ deve ter 14 números.', 'length')
