82f130b6b81dba9176fd9a69021471ddc4a08f6c
leo <leonardo.ncintra@outlook.com>
2021-03-27 17:07:14 -0300

    fix url

--- cursotiro/settings.py
+++ cursotiro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'x4&a4)ahpy$sl^^kvd1%ffl=ezh(88p)q2bdo6vhvt$t$$21)!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cursotiro-stage.herokuapp.com', 'cursotiro.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tiro-teste.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
